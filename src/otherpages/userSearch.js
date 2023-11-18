13c83342483b1930c6ab6bfa843d4186056947b2
Jarryd <136974376+Devoidsm@users.noreply.github.com>
2023-11-18 18:43:36 +0200

    Update userSearch.js
    Fixing Loading function

--- src/otherpages/userSearch.js
+++ src/otherpages/userSearch.js
@@ -1,9 +1,19 @@
 //imports 
 import React from 'react';
 import { UI, SearchgitUser, Header } from '../components/allcomp.js';
-
+import { GitInformation } from '../gitHubInfo/gitHubInfo.js';
 //function used to display the main search page
 const Main = () => {
+  const { Loading } = React.useContext(GitInformation);
+  if (Loading) {
+    return (
+      <main>
+        <Navbar />
+        <Search />
+        <img src={loadingImage} className='loading-img' alt='loding' />
+      </main>
+    );
+  }
   return (
     <main>
       <Header></Header>
