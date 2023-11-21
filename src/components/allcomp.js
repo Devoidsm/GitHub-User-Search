06ae828619c49b03a53ec637273028ca9a14366f
Jarryd <136974376+Devoidsm@users.noreply.github.com>
2023-11-21 14:59:17 +0200

    Delete src directory

--- src/components/allcomp.js
+++
@@ -1,8 +0,0 @@
-//Importing all other Components in a single file for use
-import gitRepos from "./gitRepos.js";
-import GitCommits from "./gitCommits.js";
-import UI from "./UI.js";
-import SearchgitUser from "./gitSearch.js";
-import Header from "./Header.js";
-//Exporting All components
-export { gitRepos, UI, SearchgitUser, Header, GitCommits };
